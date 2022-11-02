10bd25fdcfdad9a3b7bbc51a3b43ce6cffae4f04
tomsons6 <toms.amsons@va.lv>
2022-11-02 16:39:43 +0200

    Split Linac model, more functionality

--- Assets/_REEB/Scripts/LinacController.cs
+++ Assets/_REEB/Scripts/LinacController.cs
@@ -7,6 +7,10 @@ public class LinacController : MonoBehaviour
     [SerializeField]
     GameObject BedBase;
     [SerializeField]
+    GameObject FineLift;
+    [SerializeField]
+    GameObject Bed;
+    [SerializeField]
     GameObject LinacAparatusMovement;
     [SerializeField]
     float LinacTurnSpeed = 1.5f;
@@ -16,6 +20,18 @@ public class LinacController : MonoBehaviour
     [SerializeField]
     GameObject ButtonDown;
     ButtonPush BPDOWN;
+    [SerializeField]
+    GameObject ButtonFineDown;
+    ButtonPush BPFineDOWN;
+    [SerializeField]
+    GameObject ButtonFineUp;
+    ButtonPush BPFineUp;
+    [SerializeField]
+    GameObject ButtonMoveBedForward;
+    ButtonPush BPMoveBedForward;
+    [SerializeField]
+    GameObject ButtonMoveBedBack;
+    ButtonPush BPMoveBedBack;
     [SerializeField]
     GameObject ButtonStart;
     ButtonPush BPStart;
@@ -29,29 +45,35 @@ public class LinacController : MonoBehaviour
         BPUP = ButtonUp.GetComponent<ButtonPush>();
         BPDOWN = ButtonDown.GetComponent<ButtonPush>();
         BPStart = ButtonStart.GetComponent<ButtonPush>();
+        BPFineDOWN = ButtonFineDown.GetComponent<ButtonPush>(); 
+        BPFineUp = ButtonFineUp.GetComponent<ButtonPush>(); 
+        BPMoveBedForward = ButtonMoveBedForward.GetComponent<ButtonPush>();
+        BPMoveBedBack = ButtonMoveBedBack.GetComponent<ButtonPush>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        BedMovement();
+        BaseLift();
         AparatusMovement();
+        FineLiftMovement();
+        BedMovement();
     }
 
-    void BedMovement()
+    void BaseLift()
     {
         if (BPUP.ButtonPushed)
         {
-            if (BedBase.transform.localScale.z <= 2f)
+            if (BedBase.transform.localScale.y <= 2f)
             {
-                BedBase.transform.localScale = new Vector3(BedBase.transform.localScale.x, BedBase.transform.localScale.y, BedBase.transform.localScale.z + .001f);
+                BedBase.transform.localScale = new Vector3(BedBase.transform.localScale.x, BedBase.transform.localScale.y + .001f, BedBase.transform.localScale.z );
             }
         }
         if (BPDOWN.ButtonPushed)
         {
-            if (BedBase.transform.localScale.z >= 1f)
+            if (BedBase.transform.localScale.y >= 1f)
             {
-                BedBase.transform.localScale = new Vector3(BedBase.transform.localScale.x, BedBase.transform.localScale.y, BedBase.transform.localScale.z - .001f);
+                BedBase.transform.localScale = new Vector3(BedBase.transform.localScale.x, BedBase.transform.localScale.y - .001f, BedBase.transform.localScale.z );
             }
         }
     }
@@ -64,6 +86,40 @@ public class LinacController : MonoBehaviour
         if (StartLinac)
         {
             LinacAparatusMovement.transform.Rotate(new Vector3(1f, 0f, 0f) * LinacTurnSpeed * Time.deltaTime);
+        }
+    }
+    void FineLiftMovement()
+    {
+        if (BPFineUp.ButtonPushed)
+        {
+            if (FineLift.transform.localScale.z <= 2f)
+            {
+                FineLift.transform.localScale = new Vector3(FineLift.transform.localScale.x, FineLift.transform.localScale.y, FineLift.transform.localScale.z + .001f);
+            }
+        }
+        if (BPFineDOWN.ButtonPushed)
+        {
+            if (FineLift.transform.localScale.z >= 1f)
+            {
+                FineLift.transform.localScale = new Vector3(FineLift.transform.localScale.x, FineLift.transform.localScale.y, FineLift.transform.localScale.z - .001f);
+            }
+        }
+    }
+    void BedMovement()
+    {
+        if (BPMoveBedForward.ButtonPushed)
+        {
+            if(Bed.transform.localPosition.y <= 1130f)
+            {
+                Bed.transform.localPosition = new Vector3(Bed.transform.localPosition.x, Bed.transform.localPosition.y + .1f, Bed.transform.localPosition.z);
+            }
+        }
+        if (BPMoveBedBack.ButtonPushed)
+        {
+            if(Bed.transform.localPosition.y >= 708f)
+            {
+                Bed.transform.localPosition = new Vector3(Bed.transform.localPosition.x, Bed.transform.localPosition.y - .1f, Bed.transform.localPosition.z);
+            }
         }
     }
 }
